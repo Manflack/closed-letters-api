0b49fca01eed1bfeca01b6c4876b0511c95f0ea7
Mariano <marianofauez@live.com>
2022-08-09 23:11:27 -0300

    added morgan logger

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import cors from 'cors';
 import { analyzeWord } from './controller/WordController'
 
 const app = express();
+const morgan = require('morgan');
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan('common'))
 
 app.use('/api/v1/analyze-word', analyzeWord);
 
